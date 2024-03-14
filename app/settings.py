abcf599bc7749cedae18541e33030ad0fc9b1793
VitorinoE <128150852+VitorinoE@users.noreply.github.com>
2024-03-14 13:35:19 -0300

    fix setting 2

--- app/settings.py
+++ app/settings.py
@@ -81,28 +81,39 @@ WSGI_APPLICATION = "app.wsgi.application"
 #     }
 # }
 
-if os.getenv('GITHUB_WORKFLOW'):
-    DATABASES = {
-        "default": {
-            "ENGINE": 'django.db.backends.postgresql',
-            "NAME": 'github-actions',
-            "USER": 'postgres',
-            "PASSWORD": 'postgres',
-            "HOST": 'localhost',
-            "PORT": '5432'
-        }
+DATABASES = {
+    "default": {
+        "ENGINE": 'django.db.backends.postgresql',
+        "NAME": 'github-actions',
+        "USER": 'postgres',
+        "PASSWORD": 'postgres',
+        "HOST": 'localhost',
+        "PORT": '5432'
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": 'django.db.backends.postgresql',
-            "NAME": os.getenv('DB_NAME'),
-            "USER": os.getenv('DB_USER'),
-            "PASSWORD": os.getenv('DB_PASSWORD'),
-            "HOST": os.getenv('DB_HOST'),
-            "PORT": os.getenv('DB_PORT')
-        }
-    }
+}
+
+# if os.getenv('GITHUB_WORKFLOW'):
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": 'django.db.backends.postgresql',
+#             "NAME": 'github-actions',
+#             "USER": 'postgres',
+#             "PASSWORD": 'postgres',
+#             "HOST": 'localhost',
+#             "PORT": '5432'
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": 'django.db.backends.postgresql',
+#             "NAME": os.getenv('DB_NAME'),
+#             "USER": os.getenv('DB_USER'),
+#             "PASSWORD": os.getenv('DB_PASSWORD'),
+#             "HOST": os.getenv('DB_HOST'),
+#             "PORT": os.getenv('DB_PORT')
+#         }
+#     }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
